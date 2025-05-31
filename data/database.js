ac2f9afcf56cd9ef6e4f82907d39a63688490a23
Anand Prakash <anandprakashj9155@gmail.com>
2025-05-31 09:04:12 +0530

    fix engine

--- data/database.js
+++ data/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI, { dbName: "backendapi", })
-        .then(() => console.warn("Database Connected"))
+        .then((c) => console.warn(`Database Connected ${c.connection.host}`))
         .catch((e) => console.warn(e));
 };
